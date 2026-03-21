129783bca6da11cfb1d8f580bc9fd3c6f715b203
TieGao <eeg2332019@outlook.com>
2026-03-21 22:42:54 +0800

    Frozen Engine 0.4.7 Mobile

--- source/mobile/backend/StorageUtil.hx
+++ source/mobile/backend/StorageUtil.hx
@@ -57,44 +57,54 @@ class StorageUtil
 		return '/sdcard/.PsychEngine/';
 
 	public static function requestPermissions():Void
+{
+	if (AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU)
 	{
-		if (AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU)
-			AndroidPermissions.requestPermissions(['READ_MEDIA_IMAGES', 'READ_MEDIA_VIDEO', 'READ_MEDIA_AUDIO', 'READ_MEDIA_VISUAL_USER_SELECTED']);
-		else
-			AndroidPermissions.requestPermissions(['READ_EXTERNAL_STORAGE', 'WRITE_EXTERNAL_STORAGE']);
-
-		if (!AndroidEnvironment.isExternalStorageManager())
-			AndroidSettings.requestSetting('MANAGE_APP_ALL_FILES_ACCESS_PERMISSION');
-
-		if ((AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU
-			&& !AndroidPermissions.getGrantedPermissions().contains('android.permission.READ_MEDIA_IMAGES'))
-			|| (AndroidVersion.SDK_INT < AndroidVersionCode.TIRAMISU
-				&& !AndroidPermissions.getGrantedPermissions().contains('android.permission.READ_EXTERNAL_STORAGE')))
-			CoolUtil.showPopUp(Language.getPhrase('permissions_message', 'If you accepted the permissions you are all good!\nIf you didn\'t then expect a crash\nPress OK to see what happens'),
-				Language.getPhrase('mobile_notice', "Notice!"));
-
-		try
-		{
-			if (!FileSystem.exists(StorageUtil.getStorageDirectory()))
-				FileSystem.createDirectory(StorageUtil.getStorageDirectory());
-		}
-		catch (e:Dynamic)
-		{
-			CoolUtil.showPopUp(Language.getPhrase('create_directory_error', 'Please create directory to\n{1}\nPress OK to close the game', [StorageUtil.getStorageDirectory()]), Language.getPhrase('mobile_error', "Error!"));
-			lime.system.System.exit(1);
-		}
-
-		try
-		{
-			if (!FileSystem.exists(StorageUtil.getExternalStorageDirectory() + 'mods'))
-				FileSystem.createDirectory(StorageUtil.getExternalStorageDirectory() + 'mods');
-		}
-		catch (e:Dynamic)
-		{
-			CoolUtil.showPopUp(Language.getPhrase('create_directory_error', 'Please create directory to\n{1}\nPress OK to close the game', [StorageUtil.getExternalStorageDirectory()]), Language.getPhrase('mobile_error', "Error!"));
-			lime.system.System.exit(1);
-		}
+		// Request each permission individually
+		AndroidPermissions.requestPermission('READ_MEDIA_IMAGES');
+		AndroidPermissions.requestPermission('READ_MEDIA_VIDEO');
+		AndroidPermissions.requestPermission('READ_MEDIA_AUDIO');
+		AndroidPermissions.requestPermission('READ_MEDIA_VISUAL_USER_SELECTED');
 	}
+	else
+	{
+		// Request each permission individually
+		AndroidPermissions.requestPermission('READ_EXTERNAL_STORAGE');
+		AndroidPermissions.requestPermission('WRITE_EXTERNAL_STORAGE');
+	}
+
+	if (!AndroidEnvironment.isExternalStorageManager())
+		AndroidSettings.requestSetting('MANAGE_APP_ALL_FILES_ACCESS_PERMISSION');
+
+	if ((AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU
+		&& !AndroidPermissions.getGrantedPermissions().contains('android.permission.READ_MEDIA_IMAGES'))
+		|| (AndroidVersion.SDK_INT < AndroidVersionCode.TIRAMISU
+			&& !AndroidPermissions.getGrantedPermissions().contains('android.permission.READ_EXTERNAL_STORAGE')))
+		CoolUtil.showPopUp(Language.getPhrase('permissions_message', 'If you accepted the permissions you are all good!\nIf you didn\'t then expect a crash\nPress OK to see what happens'),
+			Language.getPhrase('mobile_notice', "Notice!"));
+
+	try
+	{
+		if (!FileSystem.exists(StorageUtil.getStorageDirectory()))
+			FileSystem.createDirectory(StorageUtil.getStorageDirectory());
+	}
+	catch (e:Dynamic)
+	{
+		CoolUtil.showPopUp(Language.getPhrase('create_directory_error', 'Please create directory to\n{1}\nPress OK to close the game', [StorageUtil.getStorageDirectory()]), Language.getPhrase('mobile_error', "Error!"));
+		lime.system.System.exit(1);
+	}
+
+	try
+	{
+		if (!FileSystem.exists(StorageUtil.getExternalStorageDirectory() + 'mods'))
+			FileSystem.createDirectory(StorageUtil.getExternalStorageDirectory() + 'mods');
+	}
+	catch (e:Dynamic)
+	{
+		CoolUtil.showPopUp(Language.getPhrase('create_directory_error', 'Please create directory to\n{1}\nPress OK to close the game', [StorageUtil.getExternalStorageDirectory()]), Language.getPhrase('mobile_error', "Error!"));
+		lime.system.System.exit(1);
+	}
+}
 	#end
 	#end
 }
