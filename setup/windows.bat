499cf3dacb495bded3264163d2051a3c8edd511b
TieGao <eeg2332019@outlook.com>
2026-04-11 09:56:08 +0800

    TEST WITH GIT VER OPENFL&LIME

--- setup/windows.bat
+++ setup/windows.bat
@@ -6,6 +6,7 @@ echo This might take a few moments depending on your internet speed.
 haxelib git hxcpp https://github.com/TieGao/hxcpp --quiet
 haxelib git lime https://github.com/openfl/lime
 haxelib git openfl https://github.com/openfl/openfl
+haxelib install format
 haxelib install flixel 6.1.0 --quiet
 haxelib install flixel-addons 4.0.1 --quiet
 haxelib install flixel-tools 1.5.1 --quiet
