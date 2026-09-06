5d6a40c504787a67f9cca16e138be687d7f68d05
TieGao <eeg2332019@outlook.com>
2026-09-06 11:57:39 +0800

    FIX FUCKING ANDROID

--- source/import.hx
+++ source/import.hx
@@ -25,6 +25,19 @@ import mobile.backend.MobileData;
 import mobile.input.MobileInputManager;
 import mobile.backend.TouchUtil;
 
+// Android
+#if Android
+import extension.androidtools.content.Context as AndroidContext;
+import extension.androidtools.widget.Toast as AndroidToast;
+import extension.androidtools.os.Environment as AndroidEnvironment;
+import extension.androidtools.Permissions as AndroidPermissions;
+import extension.androidtools.Settings as AndroidSettings;
+import extension.androidtools.Tools as AndroidTools;
+import extension.androidtools.os.Build.VERSION as AndroidVersion;
+import extension.androidtools.os.Build.VERSION_CODES as AndroidVersionCode;
+import extension.androidtools.os.BatteryManager as AndroidBatteryManager;
+#end
+
 #if sys
 import sys.*;
 import sys.io.*;
